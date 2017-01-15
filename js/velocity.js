d0a79b7cddad107696eb564dd22e2faae80dc4d6
Tushar Tyagi <tushar4r@gmail.com>
2017-01-15 23:43:06 +0530

    1 particle

--- js/velocity.js
+++ js/velocity.js
@@ -3,25 +3,20 @@ window.onload = function() {
 	context = canvas.getContext("2d"),
 	width = canvas.width = window.innerWidth,
 	height = canvas.height = window.innerHeight,
-        position = vector.create(100, 100),
-        velocity = vector.create(0, 0);
+        p = particle.create(100, 100, 0.3, Math.PI / 6);
 
-    velocity.setLength(0.3);
-    velocity.setAngle(Math.PI / 6);
-
-    
     update();
 
     function update() {
         context.clearRect(0, 0, width, height);
 
         context.beginPath();
-        context.arc(position.getX(), position.getY(), 10, 0, Math.PI * 2, false);
+        context.arc(p.position.getX(), p.position.getY(), 10, 0, Math.PI * 2, false);
         context.fill();
 
 
         // Update the position with the velocity vector, i.e. move the circle
-        position.addTo(velocity);
+        p.update();
 
         requestAnimationFrame(update);
     }
